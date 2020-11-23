67728b0f31164a1f5c5ad52eaca5c6db42135120
Derek Foster <derekfoster94@gmail.com>
2020-11-23 15:48:07 -0800

    added bcrypt to models

--- models/restaurantgoer.js
+++ models/restaurantgoer.js
@@ -1,4 +1,7 @@
 "use strict";
+
+const bcrypt = require("bcrypt");
+
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class restaurantGoer extends Model {
@@ -11,13 +14,55 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       models.restaurantGoer.hasMany(models.history);
     }
+
+    validPassword(passwordTyped) {
+      return bcrypt.compareSync(passwordTyped, this.password);
+    }
+
+    // remove the password before serializing
+    toJSON() {
+      let userData = this.get();
+      delete userData.password;
+      return userData;
+    }
   }
   restaurantGoer.init(
     {
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      firstName: DataTypes.STRING,
-      lastName: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            msg: "Invalid email address"
+          }
+        }
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [1, 99],
+            msg: "Name must be between 1 and 99 characters"
+          }
+        }
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [1, 99],
+            msg: "Name must be between 1 and 99 characters"
+          }
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [6, 99],
+            msg: "Password must be between 6 and 99 characters"
+          }
+        }
+      },
       zipCode: DataTypes.STRING,
       birthdate: DataTypes.DATE,
       diningStyle: DataTypes.STRING,
@@ -28,5 +73,14 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "restaurantGoer"
     }
   );
+
+  restaurantGoer.beforeCreate((pendingUser, options) => {
+    if (pendingUser && pendingUser.password) {
+      // hash the password
+      let hash = bcrypt.hashSync(pendingUser.password, 12);
+      // store the hash as the user's password
+      pendingUser.password = hash;
+    }
+  });
   return restaurantGoer;
 };
--- models/restaurantowner.js
+++ models/restaurantowner.js
@@ -1,4 +1,7 @@
 "use strict";
+
+const bcrypt = require("bcrypt");
+
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class restaurantOwner extends Model {
@@ -11,13 +14,56 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       models.restaurantOwner.hasMany(models.history);
     }
+
+    validPassword(passwordTyped) {
+      return bcrypt.compareSync(passwordTyped, this.password);
+    }
+
+    // remove the password before serializing
+    toJSON() {
+      let userData = this.get();
+      delete userData.password;
+      return userData;
+    }
   }
+
   restaurantOwner.init(
     {
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      firstName: DataTypes.STRING,
-      lastName: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            msg: "Invalid email address"
+          }
+        }
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [1, 99],
+            msg: "Name must be between 1 and 99 characters"
+          }
+        }
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [1, 99],
+            msg: "Name must be between 1 and 99 characters"
+          }
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [6, 99],
+            msg: "Password must be between 6 and 99 characters"
+          }
+        }
+      },
       zipCode: DataTypes.STRING,
       birthdate: DataTypes.DATE,
       description: DataTypes.STRING
@@ -27,5 +73,14 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "restaurantOwner"
     }
   );
+
+  restaurantOwner.beforeCreate((pendingUser, options) => {
+    if (pendingUser && pendingUser.password) {
+      // hash the password
+      let hash = bcrypt.hashSync(pendingUser.password, 12);
+      // store the hash as the user's password
+      pendingUser.password = hash;
+    }
+  });
   return restaurantOwner;
 };
